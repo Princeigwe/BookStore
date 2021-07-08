ac119d7c1cd6bb915180be56dd1c3d0df2bd9b7c
Prince Igwenagha <Igwep297@gmail.com>
2021-07-08 13:51:43 +0100

    added environment variables

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-rn%f8dsn4qey_9-_7+3&ukru2+th&q@c@y7pn@+d3gyhcohpn'
+SECRET_KEY = os.environ.get('NEW_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default = 0))
 
 ALLOWED_HOSTS = ['0.0.0.0']
 
