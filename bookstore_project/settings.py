024e282e972e93c925eabc5ff61ccdd09d4eb2ad
Prince Igwenagha <Igwep297@gmail.com>
2021-08-11 04:48:03 +0100

    added env file

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+# for HEROKU
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
