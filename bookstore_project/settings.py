cc3a2350d005876c5e7f255e09114900848187ec
Prince Igwenagha <Igwep297@gmail.com>
2021-08-11 15:10:07 +0100

    added static files storage

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -178,6 +178,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder"
