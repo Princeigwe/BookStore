5653cf9cba7d0364901d59b2b39ff386902a9560
Prince Igwenagha <Igwep297@gmail.com>
2021-08-11 16:20:31 +0100

    whitenoise setting

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -178,7 +178,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
