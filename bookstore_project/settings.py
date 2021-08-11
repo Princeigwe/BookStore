a56486644bdbface3251da833a6ff9fc0d8973cf
Prince Igwenagha <Igwep297@gmail.com>
2021-08-11 05:43:27 +0100

    prevented redirects

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -31,6 +31,7 @@ if ENVIRONMENT == 'production':
     SECURE_CONTENT_TYPE_NOSNIFF = True 
     SESSION_COOKIE_SECURE=True
     CSRF_COOKIE_SECURE=True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https') ## to prevent redirects
 
 
 # Quick-start development settings - unsuitable for production
