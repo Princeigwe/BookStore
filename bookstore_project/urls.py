509fef1a0547976cdb8c76e483d2eba33b7bc972
Prince Igwenagha <Igwep297@gmail.com>
2021-08-11 20:27:32 +0100

    made changes in urls.py

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -59,7 +59,10 @@ urlpatterns = [
     path('swagger/doc/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/doc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) ## adding the media url and it's root
+] 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                        document_root=settings.MEDIA_ROOT)## adding the media url and it's root
 
 
 admin.site.site_header  =  "BookStore Admin"  # Custom BookStore Admin Header
