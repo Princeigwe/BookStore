8973f0cde32b8b619936fa866dd7f139320e0b85
Prince Igwenagha <Igwep297@gmail.com>
2021-08-11 05:09:27 +0100

    added database

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -14,8 +14,6 @@ import os
 
 # for HEROKU
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -133,6 +131,9 @@ DATABASES = {
         'PORT': 5432
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
