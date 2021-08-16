53996527e0db98623f7691061cf3fbd71b4a011e
Prince Igwenagha <Igwep297@gmail.com>
2021-08-16 15:09:37 +0100

    added storages to apps

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -74,6 +74,7 @@ INSTALLED_APPS = [
     'dj_rest_auth.registration', # django restframework authentication app for registration
     'drf_yasg', # rest app for api documentation
     'django_dropbox_storage', # django dropbox storage... forked version
+    'storages', ## django storages
     
     
     
